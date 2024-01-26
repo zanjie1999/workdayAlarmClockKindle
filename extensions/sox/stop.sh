0f580fa74a838d1c5d4ff623fa6e7f22f0f0c658
zanjie1999 <zanjie1999@yeah.net>
2024-01-26 10:09:36 +0800

    播放本地文件

--- extensions/sox/stop.sh
+++ extensions/sox/stop.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # zyyme 240125
 killall workdayAlarmClock-linux-arm
+killall find
 pkill -9 gst-launch
 pkill -9 sox
--- extensions/sox/stopPlay.sh
+++ extensions/sox/stopPlay.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # zyyme 240125
-curl http://127.0.0.1:8080/stop
+curl http://127.0.0.1:8080/stop -m 1
+killall find
 pkill -9 gst-launch
 pkill -9 sox
